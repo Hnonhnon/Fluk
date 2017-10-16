1f8e3b0a893d55252d626b3063e8075c6dd1c2cc
Hnonhnon <32641580+Hnonhnon@users.noreply.github.com>
2017-10-16 13:10:57 +0700

    fluk

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "C:\\Users\\train05\\fluk\\nextjs-tutorial\\pages\\index.js?entry";
+
+
+var Index = function Index() {
+    return _react2.default.createElement("hl", {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 4
+        }
+    }, "this is SIT CRAFT Camp ");
+};
+
+exports.default = Index;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxpbmRleC5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIkluZGV4Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPOzs7Ozs7Ozs7QUFFUCxJQUFNLFFBQVEsU0FBUixBQUFRLFFBQUE7MkJBQ1YsY0FBQTs7c0JBQUE7d0JBQUE7QUFBQTtBQUFBLEtBQUEsRUFEVSxBQUNWO0FBREosQUFJQTs7a0JBQUEsQUFBZSIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiJDOi9Vc2Vycy90cmFpbjA1L2ZsdWsvbmV4dGpzLXR1dG9yaWFsIn0=
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Index = () => (
+    <hl>this is SIT CRAFT Camp </hl>
+)
+
+export default Index
